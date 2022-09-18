230a37b4c2c05ec14d1885d5fae0cf7e27284507
Ruben Vicente Arevalo <ruben1.6180@gmail.com>
2022-09-18 22:50:23 +0200

    general improvements in the task 2

--- create_datasets.py
+++ create_datasets.py
@@ -3,79 +3,104 @@ import pandas as pd
 import numpy as np
 
 import eikon as ek
-import Claves
+import claves
 
 import datetime
 import os
 
 np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)
 
-ek.set_app_key(Claves.API_key)
+ek.set_app_key(claves.API_key)
 
 
-def download_hist(name_file,RIC_dates):
+def download_hist(RIC,start_date,end_date, name_file,RIC_dates,now,interval='daily'):
     """
-    If the data is not already downloaded, it downloads the data and save it as csv
+    If the data is not already downloaded, it downloads the data and save it as xlsx
+    parameters:
+    fields = # 'VALUE', 'VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE', 'COUNT' By default all fields are returned.
+    interval= 'tick', 'minute', 'hour', 'daily', 'weekly', 'monthly', 'quarterly', 'yearly' (Default 'daily') Default: 'daily'
     """
     if os.path.exists(name_file):
-        df = pd.read_excel(name_file)
-        print("Datos ya existentes:", RIC_dates)
-        return df
+        log("Datos ya existentes:"+RIC_dates,now)
+        return pd.read_csv(name_file)
     else:
         try:
             df = ek.get_timeseries(rics=RIC,
-                                    fields='*',  # 'VALUE', 'VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE', 'COUNT' By default all fields are returned.
+                                    fields='*',
                                     start_date=str(start_date),
-                                    end_date=str(end_date))
-                                    #interval='daily')  # 'tick', 'minute', 'hour', 'daily', 'weekly', 'monthly', 'quarterly', 'yearly' (Default 'daily') Default: 'daily'
-            if df.shape[0]<400:
-                return None
-            df.to_excel(name_file)
-            print(f"Size:{df.shape[0]} --> Download:", RIC_dates)
+                                    end_date=str(end_date),
+                                    interval=interval) 
+            df.to_csv(name_file)
+            log(f"Downloaded and save:{RIC_dates}\nSize:{df.shape[0]}",now)
             return df
         except Exception as e:
             print(e)
+            log(str(e),now)
             return None
 
-def save_hist(df,name_file, min_T = 10,max_T = 20):
-    if not os.path.exists(name_file):
-        # Random size of the vector
-        len_route = np.random.randint(min_T,max_T+1,int(df.shape[0]/min_T))
-        # Start with a 0 and create a cum sum vector to obtain the postions
-        pos_df = np.insert(len_route, 0, 0).cumsum()
-        # Only use the postions lower than the size
-        pos_df = pos_df[pos_df<df.shape[0]-np.random.randint(min_T,max_T+1)]
-        # The last postion will the the size of the vector
-        pos_df = np.append(pos_df, [df.shape[0]-1])
-        # Chop the closing price with the previous random positions
-        trajectories = np.array([np.array(df['CLOSE'][pos_df[i]:pos_df[i+1]]) for i in range(len(pos_df)-1)])
-        # Compute the returns with the x0 as the base
-        norm_trajectories = [np.array((x/x[0]-1)*100).tolist() for x in trajectories]
-        #np.savetxt('text.txt',norm_trajectories,fmt='%1.5f',delimiter=';')
+def save_hist(df,name_file, now, min_T = 10,max_T = 20):
+    """
+    Clean the df from NA and compute the log returns
+    """
+    size_df = len(df)
+    df.dropna(subset=['CLOSE'], inplace=True)
+    
+    if size_df != len(df):
+        log(str(size_df-len(df))+' rows with NA',now)
+    
+    
+    # Define the random size of the vector
+    len_route = np.random.randint(min_T,max_T+1,int(df.shape[0]/min_T))
+    # Start with a 0 and create a cum sum vector to obtain the postions
+    pos_df = np.insert(len_route, 0, 0).cumsum()
+    # Only use the postions lower than the size
+    pos_df = pos_df[pos_df<df.shape[0]-np.random.randint(min_T,max_T+1)]
+    # The last postion will the the size of the vector
+    pos_df = np.append(pos_df, [df.shape[0]-1])
+    # Chop the closing price with the random positions
+    trajectories = np.array([np.array(df['CLOSE'][pos_df[i]:pos_df[i+1]]) for i in range(len(pos_df)-1)])
+    # Compute the returns compare with the x_0
+    norm_trajectories = []
+    for x in trajectories:
+        norm_trajectories.append([np.log(_/x[0])*100 for _ in x])
+    #np.savetxt('text.txt',norm_trajectories,fmt='%1.5f',delimiter=';')
 
-        # Save it a txt
-        with open(name_file,'w') as f:
-            for trajectory in norm_trajectories:
-                f.write('1.0;'+';'.join([str(x) for x in trajectory])+'\n')
+    if not all(isinstance(_,float) for _ in sum(norm_trajectories,[])):
+        log("Error in the returns of:"+name_file, now)
+    
+    # Save the retuns in a txt
+    with open(name_file,'w') as f:
+        for trajectory in norm_trajectories:
+            f.write('1.0;'+';'.join([str(x) for x in trajectory])+'\n')
+    
+    log("Returns compute",now)
+
+def log(txt,now):
+    f = open(__file__[:-3]+'_'+now+'.log', 'a')
+    f.write(txt + '\r')
+    f.close()
 
 if __name__ == '__main__':
 
-    start_date = datetime.datetime(year=2020,month=7,day=1)
+    start_date = datetime.datetime(year=2012,month=7,day=1)
     end_date = datetime.datetime(year=2022,month=7,day=1)
-
+    now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    data = 'analisis/S&P_500.xlsx'
+    
     df = pd.read_excel('analisis/S&P_500.xlsx')
+    len_min = 10
+    len_max = 20
+
+    df = pd.read_excel(data)
     RICs = df['Identifier'].to_list()
+    
     for RIC in RICs:    
-        if RIC not in ['ACLT.DH','ENER.DH','NABV.NS','QUER.MOT','MLECO.EUA','ABENU.PK','EJGJ.SJ']:
-            print(RIC)
-            RIC_dates = RIC + '-' + str(start_date.date()) + '-' + str(end_date.date())
-        
-            name_file_xlsx = os.path.join('data' , RIC_dates + '.xlsx')
-            df = download_hist(name_file_xlsx,RIC_dates)
-
-            if df is not None and np.sum(np.isnan(df['CLOSE'])) == 0:
-                
-                name_file_txt = os.path.join('data' , RIC_dates + '.txt')
-                save_hist(df,name_file_txt)
-                print(f'Done')
-
+        log(RIC,now)
+        RIC_dates = RIC + '-' + str(start_date.date()) + '-' + str(end_date.date())
+        name_file = os.path.join('data' , RIC_dates + '.csv')
+        df = download_hist(RIC,start_date,end_date, name_file,RIC_dates)
+            
+        if df is not None:    
+            name_file_txt = os.path.join('data' , f"{RIC_dates}-{len_min}-{len_max}.txt")
+            save_hist(df,name_file_txt, min_T = len_min,max_T = len_max)
+            log('Done',now)
--- load_run_models.py
+++ load_run_models.py
@@ -8,38 +8,69 @@ from keras.models import load_model
 from keras.utils import pad_sequences
 
 import eikon as ek
-import Claves
+import claves
+from create_datasets import log
 
-
-def read_reshape_txt(file):
+def read_reshape_txt(file,max_length):
     with open(file, "r") as f:    
-        task = [np.array(x.split(";"), dtype="float64") for x in f.readlines()]
+        task = [np.array(x.split(";"), dtype="float32") for x in f.readlines()]
         task = np.array([x[1:] for x in task])
+        
+        """import andi
+        AD = andi.andi_datasets()
+        dataset = AD.andi_dataset(N = 1_000, tasks = 2, dimensions = 1)
+        task = np.array([x[1:] for x in [np.array(dataset[2][0][0]),np.array(dataset[2][0][1])]])
+        """
         task = task[:,]
-    max_length = 20
-    task_HomLen = pad_sequences(task, maxlen=max_length, dtype="float64")
+    
+    task_HomLen = pad_sequences(task, maxlen=max_length, dtype="float32")
     return task_HomLen.reshape((-1, max_length, 1)) # reshape all to the same size of max_lenght
 
-def predict(model,df,dates):
-    df['avg_exp'] = ""
+def predict_task1(model,df,start_date, end_date,task,len_min,len_max,now):
+    returns_str = f"{start_date.date()}-{end_date.date()}-{len_min}-{len_max}"
+
+    df[f'task{task}_len_{len_min}_{len_max}'] = ""
 
     for i,RIC in enumerate(df['Identifier']):
-        RIC_dates = RIC + '-' + dates
-        name_file_txt = os.path.join('data' , RIC_dates + '.txt')
+        RIC_dates = RIC + '-' + returns_str + '.txt'
+        name_file_txt = os.path.join('data' , RIC_dates)
         
-        if  RIC_dates + '.txt' in os.listdir('data'):
-            print(RIC)
+        if  RIC_dates in os.listdir('data'):
+            log(RIC+' read',now)
             
-            task_HomLenRes = read_reshape_txt(name_file_txt)
+            task_HomLenRes = read_reshape_txt(name_file_txt,len_max)
             
             # Clasified the data
             exps = model.predict(task_HomLenRes)
+            # Averaged all the exponents in the trayectorias
             df.iloc[i,-1] = np.mean(exps)
+            log(RIC+' predicted and save',now)
+    return df
+
+def predict_task2(model,df,start_date, end_date,task,len_min,len_max,now):
+    returns_str = f"{start_date.date()}-{end_date.date()}-{len_min}-{len_max}"
+
+    df[f'task{task}_len_{len_min}_{len_max}'] = ""
+
+    for i,RIC in enumerate(df['Identifier']):
+        RIC_dates = RIC + '-' + returns_str + '.txt'
+        name_file_txt = os.path.join('data' , RIC_dates)
+        
+        if  RIC_dates in os.listdir('data'):
+            log(RIC+' read',now)
+            
+            task_HomLenRes = read_reshape_txt(name_file_txt,len_max)
+            
+            # Clasified the data
+            model_predicted = model.predict(task_HomLenRes) # XXXX aqui estan los NA
+            
+            df.iloc[i,-1] = model_predicted
+            log(RIC+' predicted and save',now)
     return df
 
 def download_ATM_vol(df,start_date,end_date,excel_data):
     
-    ek.set_app_key(Claves.API_key)
+    ek.set_app_key(claves.API_key)
     sufix_implied_vol = 'ATMIV'
     df['Hist_Vol'] = ""
     df['Impl_Vol'] = ""
@@ -78,19 +109,40 @@ def download_ATM_vol(df,start_date,end_date,excel_data):
 
 
 if __name__ == '__main__':
-    # Load the model
-    model = load_model('ANDI_Challenge/models/task1/task1_len_10_20.h5')
 
     # Load the data and prepare
-    start_date = datetime.datetime(year=2020,month=7,day=1)
+    start_date = datetime.datetime(year=2012,month=7,day=1)
     end_date = datetime.datetime(year=2022,month=7,day=1)
-    excel_data = 'analisis/S&P_500.xlsx'
+    now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    task = 2
+    path = 'ANDI_Challenge/models/task'+str(task)
+    data = 'analisis/S&P_500_analisis.xlsx'
 
-    df = pd.read_excel(excel_data)
-    dates_str = str(start_date.date()) + '-' + str(end_date.date())
-    #df = predict(df,dates_str)
-    #df.to_excel(excel_data)
-    df = download_ATM_vol(df,start_date ,end_date,excel_data.replace('500','500_analisis'))
+    
+    """for len_min,len_max in zip([10,100,400,800],[20,200,500,1_000]):
+        name_model = f'task{task}_len_{len_min}_{len_max}.h5'
+        df = pd.read_excel(data,index_col=0)
+        # Load the data and prepare
+        model = load_model(os.path.join(path,name_model))
+
+        # Make predictions
+        df = predict(model,df,start_date, end_date,task,len_min,len_max,now)
+        df.to_excel(data)
+        print(len_min,'-',len_max)
+    """
+    len_min,len_max = 800,1_000
+    name_model = f'task{task}_dim{1}.h5'
+    df = pd.read_excel(data,index_col=0)
+        
+    # Load the data and prepare
+    model = load_model(os.path.join(path,name_model))
+
+    # Make predictions
+    df = predict_task2(model,df,start_date, end_date,task,len_min,len_max,now)
+    df.to_excel(data)
+    print(len_min,'-',len_max)
+        
+    #df = download_ATM_vol(df,start_date ,end_date,excel_data.replace('500','500_analisis'))
 
 
 
